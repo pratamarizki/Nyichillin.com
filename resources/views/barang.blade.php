2cc37dd4eb36c3103e1882acd33c8a082e1a4094
rizalaffandy <rizalaffandy21.if@gmail.com>
2018-12-30 14:13:30 +0700

    commit

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
-@section('content')
+@section ('content')
 <a id="cicil" type="button" class="btn btn-warning" href="{{url('cicil')}}">Cicil Sekarang</a>
 	<table>
 		<tr>
